1ffc8469fac1c787803bfe86ac26f1a7f62649e7
zsuzsa <zsuzsa.menyhart@gmail.com>
2016-03-26 15:47:37 +0200

    fixed redirect issue

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
